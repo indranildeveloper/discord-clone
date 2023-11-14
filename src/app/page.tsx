5a29a8c78a8174208ad83faa655d6e8d3f1843f4
Indranil Halder <73965619+indranildeveloper@users.noreply.github.com>
2023-11-14 14:22:31 +0530

    shadcn ui setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 import { FC } from "react";
+import { Button } from "@/components/ui/button";
 
 const HomePage: FC = () => {
-  return <div>Home Page</div>;
+  return (
+    <div className="container">
+      <h2 className="text-2xl">Home Page</h2>
+      <Button>Click Me</Button>
+    </div>
+  );
 };
 
 export default HomePage;
