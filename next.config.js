240486698bc2c18f6b1d63f9ceab8fb9f9985a12
Indranil Halder <indranilhalder.dev@gmail.com>
2024-01-08 17:16:14 +0530

    Updated config for deployment

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
+  webpack: (config, { isServer }) => {
+    config.resolve.alias["~"] = path.join(__dirname, "src");
+    return config;
+  },
   images: {
     remotePatterns: [
       {
